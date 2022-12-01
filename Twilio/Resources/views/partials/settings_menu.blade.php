b3b93ee859513837b35e909ddff8d502438eef73
Caleb <caleb.baker194@gmail.com>
2022-12-01 08:34:24 -0600

    Delete nest
    Somehow Twilio Got nested inside

--- Twilio/Resources/views/partials/settings_menu.blade.php
+++
@@ -1,5 +0,0 @@
-<li @if (\App\Misc\Helper::isMenuSelected('twilio'))class="active" @endif>
-  <a href="{{ route('mailboxes.twilio.settings', ['mailbox_id'=>$mailbox->id]) }}">
-    <i class="glyphicon glyphicon-phone"></i> {{ __('Twilio') }}
-  </a>
-</li>
